d7e4626b23e175ce38388eebbe6ac20663db5ea1
Daniel <danpellegrino1@gmail.com>
2023-05-23 15:36:32 -0400

    added ArtixPreInstall.sh

---
+++ ArtixPreInstall.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Daniel Pellegrino Artix Pre-Install runit Script
+# by Daniel Pellegrino
+# License: GNU GPLv3
+
+### FUNCTIONS ###
+
+installpkg() {
+	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
+}
+
+error() {
+	# Log to stderr and exit with failure.
+	printf "%s\n" "$1" >&2
+	exit 1
+}
+
+welcomemsg() {
+	whiptail --title "Welcome!" \
+		--msgbox "Welcome to Daniel's Pre-Install Script!\\n\\nThis script will automatically set up encryption and a btrfs file system.\\n\\n-Daniel" 10 60
+
+	whiptail --title "Important Note!" --yes-button "All ready!" \
+		--no-button "Return..." \
+		--yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
+}
+
+### THE ACTUAL SCRIPT ###
+
+pacman --noconfirm --needed -Sy libnewt ||
+	error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
+
+# Welcome user
+welcomemsg || error "User exited."
