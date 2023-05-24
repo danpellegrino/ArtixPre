1a325981ea0f75349c728987b4702084adb688ed
Daniel <danpellegrino1@gmail.com>
2023-05-24 02:08:00 -0400

    updated ArtixPre.sh

--- ArtixPreInstall.sh
+++ ArtixPreInstall.sh
@@ -177,7 +177,7 @@ setupgrub(){
 
 	mkdir /mnt/boot/efi
 	artix-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
-	grub-mkconfig -o /boot/grub/grub.cfg
+	artix-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 ### THE ACTUAL SCRIPT ###
